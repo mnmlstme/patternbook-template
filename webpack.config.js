b69fe47fa9d3ef25fc4b2afdf6ba4edf03222323
Ken Kubiak <ken@mnmlst.me>
2017-09-10 10:23:46 -0700

    add stage-0 to handle modules properly

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     query: {
-                        presets: ['react', 'es2015']
+                        presets: ['react', 'es2015', 'stage-0']
                     }
                 }
             },
@@ -41,7 +41,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         query: {
-                            presets: ['react', 'es2015']
+                            presets: ['react', 'es2015', 'stage-0']
                         }
                     },
                     {
